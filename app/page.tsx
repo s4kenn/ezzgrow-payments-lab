71d2955f455d599ee526ddaf8f9f3e89d811ca63
Aditya Singh <118333811+s4kenn@users.noreply.github.com>
2024-04-03 01:27:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,17 @@ export default async function Home() {
     return redirect("/dashboard");
   }
   return (
-    <section className="flex items-center justify-center bg-background h-[90vh]">
+     <section className="flex items-center justify-center bg-background h-[90vh]">
       <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
         <div className="max-w-3xl mx-auto text-center">
           <div>
             <span className="w-auto px-6 py-3 rounded-full bg-secondary">
               <span className="text-sm font-medium text-primary">
-                Sort your notes easily
+                Are you ready?
               </span>
             </span>
             <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
-              Create Notes with ease
+              Elevate your online presence
             </h1>
             <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
               We just want to keep you engaged :P
